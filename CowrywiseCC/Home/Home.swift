ba10f3660dcee8268608f5df12fa66876f72cd3d
Akin <rapiddevelopa@gmail.com>
2020-12-17 17:52:37 +0100

    home top down comp setup

---
+++ CowrywiseCC/Home/Home.swift
@@ -0,0 +1,60 @@
+//
+//  Home.swift
+//  CowrywiseCC
+//
+//  Created by Admin on 12/17/20.
+//  Copyright © 2020 rapid interactive. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Menu")
+                Text("Sign Up")
+            }
+            Text("Currency Calculator")
+            TextField("", text: .constant("Base"))
+            TextField("", text: .constant("Target"))
+            HStack {
+               CurrencyBtn()
+               CurrencyBtn()
+            }
+            Button(action: {
+                
+            }, label: {
+                Text("Convert")
+            })
+            HStack {
+                Text("link")
+                Image(systemName: ".info")
+            }
+            CurrencyHistoryTrend()
+            Spacer()
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+struct CurrencyBtn: View {
+     
+    var body: some View {
+        Text("Currency Btn")
+    }
+}
+
+struct CurrencyHistoryTrend: View {
+     
+    var body: some View {
+        Text("Currency Btn")
+    }
+}
+
+ 
